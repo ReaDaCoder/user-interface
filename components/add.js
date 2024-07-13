f79dba823ecd042192c77ebde1c929737cce8a34
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-07-13 11:05:47 +0200

    Added content for interface

---
+++ components/add.js
@@ -0,0 +1,11 @@
+function Interface(){
+    return(
+        <di>
+            <h1>Life Planning, Making Easy to Turn Dreams a Reality.</h1>
+            <h3>Get Exclusive offers ob purchase of any plans</h3>
+            <input type="text" placeholder="Your Email"/>
+            <button>Sign up</button>
+            <h4>Featured Plans</h4>
+        </di>
+    );
+}
