e78c62873c6318024e1a84a82f6eb5f386c29264
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-07-13 11:13:39 +0200

    created container and footer

--- components/add.js
+++ components/add.js
@@ -6,6 +6,17 @@ function Interface(){
             <input type="text" placeholder="Your Email"/>
             <button>Sign up</button>
             <h4>Featured Plans</h4>
+            <div class="container">
+                <div class="box" id="box1">1</div>
+                <div class="box" id="box2">2</div>
+                <div class="box" id="box3">3</div>
+                <div class="box" id="box4">4</div>
+                <hr/>
+                <footer>
+                    <div>Terms Policy</div>
+                    <div>Customer Story</div>
+                </footer>
+            </div>
         </di>
     );
 }
