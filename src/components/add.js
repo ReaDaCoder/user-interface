93f80da7235d3d9f8e742657578799e4a93f7019
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-07-14 10:57:09 +0200

    Added css for

--- src/components/add.js
+++ src/components/add.js
@@ -3,19 +3,43 @@ import{useState} from "react"
 function Interface(){
     return(
         <div >
+            <header>
+                <button class="login">Log In</button>
+                <button class="getstarted">Get Started</button>
+            </header>
             <h1>Life Planning, Making Easy to Turn Dreams a Reality.</h1>
-            <h3>Get Exclusive offers ob purchase of any plans</h3>
+            <h3>Get Exclusive offers on purchase of any plans</h3>
             <input type="text" placeholder="Your Email"/>
-            <button>Sign up</button>
+            <button class="signbtn">Sign up</button>
             <h4>Featured Plans</h4>
             <div class="box-grid">
-                <div class="box" id="box1">1</div>
-                <div class="box" id="box2">2</div>
-                <div class="box" id="box3">3</div>
-                <div class="box" id="box4">4</div>
-                <hr/>
+                <div class="box" id="box1">
+                    <div>Strategy</div>
+                    <h5>Money Momentum</h5>
+                    <p>Tract premium helps you start saver investments insights covering</p>
+                    <hr/>
+                </div>
+                <div class="box" id="box2">
+                    <div>Plans</div>
+                    <h5>Long Term</h5>
+                    <p>Create a vision. To get started, imagine your dream life.</p>
+                    <hr/>
+                </div>
+                <div class="box" id="box3">
+                    <div>Strategy</div>
+                    <h5>Focussed</h5>
+                    <p>Be Focussed Life Planning, however, can serve as a roadmap or guide</p>
+                    <hr/>
+                </div>
+                <div class="box" id="box4">
+                    <div>Plans</div>
+                    <h5>Fixed Income</h5>
+                    <p>Get multiple benefits, and a lumpsum option & Guaranteed Returns</p>
+                    <hr/>
+                </div>
             </div>
             <footer>
+                <hr/>
                     <div>Terms Policy</div>
                     <div>Customer Story</div>
                 </footer>
