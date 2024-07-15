b19166fd39ad7450c6277a205b08a03fde0eb0ef
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-07-16 00:08:29 +0200

    removed bootstrap link

--- src/components/add.js
+++ src/components/add.js
@@ -16,45 +16,47 @@ function Interface(){
             <button class="signbtn">Sign up</button>
             <div class="featured">Featured Plans</div>
             <div class="explore">Explore all</div>
-            <div class="box-grid">
-                <div class="col-md-4 box" id="box1">
-                    <div>Strategy</div>
-                    <h5>Money Momentum</h5>
+            <div class="wrapper">
+                <div class="box" id="box1">
+                <h5>Money Momentum</h5>
                     <p>Tract premium helps you start saver investments insights covering</p>
                     <hr/>
                     <div class="percentage">7.20%</div>
                     <img src="/media/arrow-square-right.png"alt="logo"/>
                 </div>
-                <div class="col-md-4 box" id="box2">
-                    <div>Plans</div>
+                <div class="box" id="box2">
+                <div>Plans</div>
                     <h5>Long Term</h5>
                     <p>Create a vision. To get started, imagine your dream life.</p>
                     <hr/>
                     <div class="percentage">14.20%</div>
-                    <img src="/media/arrow-square-right.png"/>
+                    <img src="/media/arrow-square-right.png" class="blackbox"/>
+
                 </div>
-                <div class="col-md-4 box" id="box3">
-                    <div>Strategy</div>
+                <div class="box" id="box3">
+                <div>Strategy</div>
                     <h5>Focussed</h5>
                     <p>Be Focussed Life Planning, however, can serve as a roadmap or guide</p>
                     <hr/>
                     <div class="percentage">11.20%</div>
                     <img src="/media/arrow-square-right.png"/>
+
                 </div>
-                <div class="col-md-4 box" id="box4">
-                    <div>Plans</div>
+                < div class="box" id="box4">
+                <div>Plans</div>
                     <h5>Fixed Income</h5>
                     <p>Get multiple benefits, and a lumpsum option & Guaranteed Returns</p>
                     <hr/>
                     <div class="percentage">8.20%</div>
                     <img src="/media/arrow-square-right.png"/>
+
                 </div>
             </div>
             <footer>
                 <hr/>
                     <div>Terms Policy</div>
                     <div>Customer Story</div>
-                    <img src="/media/arrow-square-right.png" class="bottom-arrow"/>
+                    <img src="/media/icon-arrow.svg" class="bottom-arrow"/>
                 </footer>
         </div>
     );
